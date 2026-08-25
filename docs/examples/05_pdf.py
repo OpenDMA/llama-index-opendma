520064e049254d7e838aba1e641038ab184c4824
Stefan Kopf <s.kopf@xaldon.de>
2026-08-25 20:01:06 +0200

    Revert previous fix

--- docs/examples/05_pdf.py
+++ docs/examples/05_pdf.py
@@ -38,9 +38,6 @@ reader = OpenDMAReader(
     password="ignored",
     repository_id="sample-repo",
     document_ids=["opendma-spec-document"],
-    file_extractor_per_mimetype={
-        "application/pdf": PDFReader(),
-    },
 )
 
 # Load documents
